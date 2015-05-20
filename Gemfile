299c802fa42963c7cd824291986c5224cff108ef
superandrew123 <andrewmiksch@gmail.com>
2015-05-20 13:26:16 -0400

    added database_cleaner, now testing log in system and redirect for clicking links that require a current_user

--- Gemfile
+++ Gemfile
@@ -30,9 +30,10 @@ group :development, :test do
   gem 'byebug'
   gem 'rspec-rails'
   gem 'capybara'
+  gem 'selenium-webdriver'
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
-
+  gem 'database_cleaner'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,17 +1,36 @@
 require 'rails_helper'
 
-describe "Visit default home page,", type: :feature do
-  context "no user signed in," do 
+describe "Visit /,", type: :feature do
+  before(:each) do
     Game.create(user_id: 1, home_name: "People's Court", away_name: "Bad Guys")
+    @user = User.create(email: "test@email.net", password: "password")
+  end
+
+  context "no user signed in," do 
     it "opens homepage and shows all games" do
       visit root_path
       expect(page).to have_content("Games:")
+      expect(page).to have_content("People's Court")
+      expect(page).to have_content("Bad Guys")
+      expect(page).to have_content("Resume")
     end
 
-    it "shows all games" do
+    it "cannot resume games without being logged in" do
       visit root_path
-      expect(page).to have_content("People's Court")
+      click_link('Resume', match: :first)
+      expect(current_path).to eq("/users/sign_in")
     end
   end
 
+
+  context "user signs in," do
+    it "clicks log in and goes to the log in page" do 
+      visit root_path
+      click_link('log in')
+      fill_in('Email', with: @user.email)
+      fill_in('Password', with: @user.password)
+      find('[value="Log in"]').click
+      expect(page).to have_content("New Game")
+    end
+  end
 end
