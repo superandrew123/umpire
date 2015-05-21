c1c30abdcc4c035179b33e41a436b0132a730701
superandrew123 <andrewmiksch@gmail.com>
2015-05-21 14:24:45 -0400

    Users can only resume their games

--- app/helpers/view_helper.rb
+++ app/helpers/view_helper.rb
@@ -1,7 +1,7 @@
 module ViewHelper
 
   def resume_watch(game)
-    if current_user
+    if current_user && current_user.id == game.user_id
       link_to("Resume", game_path(game))
     else
       # this needs to go to a different path where no JS will be active
--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -5,7 +5,10 @@ require 'rails_helper'
 describe "Visit /,", type: :feature do
   before(:each) do
     Game.create(user_id: 1, home_name: "People's Court", away_name: "Bad Guys")
-    @user = User.create(email: "test@email.net", password: "password")
+    Game.create(user_id: 2, home_name: "Home", away_name: "Away")
+    @user1 = User.create(email: "test1@email.net", password: "password")
+    @user2 = User.create(email: "test2@email.net", password: "password")
+    @user3 = User.create(email: "test3@email.net", password: "password")
   end
 
   context "no user signed in," do
@@ -17,6 +20,7 @@ describe "Visit /,", type: :feature do
         expect(page).to have_content("Bad Guys")
       end
     end
+
     describe "cannot resume games without being logged in" do
       it "resume is gone" do 
         visit root_path
@@ -38,12 +42,25 @@ describe "Visit /,", type: :feature do
     end
 
     it "logs in and goes back to root page and show the New Game and log out link" do
-      log_in(@user)
+      log_in(@user1)
       expect(current_path).to eq("/")
       expect(page).to have_content("New Game")
-      expect(page).to have_content("Resume")
       expect(page).to_not have_content("log in")
       expect(page).to have_content("log out")
+    end
+
+    it "shows Resume for user's games" do 
+      log_in(@user2)
+      expect(current_path).to eq("/")
+      expect(page).to have_content("Resume")
+      expect(page).to have_content("Watch")
+    end
+
+    it "shows only Watch if there are no games by the user" do
+      log_in(@user3)
+      expect(current_path).to eq("/")
+      expect(page).to_not have_content("Resume")
+      expect(page).to have_content("Watch")
     end
   end
 end
