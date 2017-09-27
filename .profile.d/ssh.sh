156263b69d9e7585580025a4f8bb45da825f3cce
superandrew123 <andrewmiksch@gmail.com>
2017-09-27 11:08:20 -0400

    #yolo push

--- .profile.d/ssh.sh
+++ .profile.d/ssh.sh
@@ -1,5 +1,12 @@
 # open ssh connection to EC2 instance
 
-# ssh -o "StrictHostKeyChecking=no" -L 6379:fohr-andrew-test-2.dpdkrj.0001.use1.cache.amazonaws.com:6379 -i ./fohr_andrew.pem "UserKnownHostsFile=/dev/null" ubuntu@ec2-54-235-5-112.compute-1.amazonaws.com
-echo $REDIS_ARM
+touch $1/fohr_andrew.pem
+
+echo "-----BEGIN RSA PRIVATE KEY-----" >> $1/fohr_andrew.pem
+cat $REDIS_ARM >> $1/fohr_andrew.pem
+echo "" >> $1/fohr_andrew.pem
+echo "-----END RSA PRIVATE KEY-----" >> $1/fohr_andrew.pem
+echo "SSH set-up complete 👍 🤘 👌"
+
+ssh -o "StrictHostKeyChecking=no" -L 6379:fohr-andrew-test-2.dpdkrj.0001.use1.cache.amazonaws.com:6379 -i ./fohr_andrew.pem ubuntu@ec2-54-235-5-112.compute-1.amazonaws.com
 echo "<---- SSHed ---->"
