1c4a0e94767be2dbf1bf1905d33ce33451002943
superandrew123 <andrewmiksch@gmail.com>
2017-09-27 11:29:07 -0400

    #yolo push

--- .profile.d/ssh.sh
+++ .profile.d/ssh.sh
@@ -1,11 +1,11 @@
 # open ssh connection to EC2 instance
 
 touch ./fohr_andrew.pem
-chmod 777 ./fohr_andrew.pem
-echo "-----BEGIN RSA PRIVATE KEY-----" >> $1/fohr_andrew.pem
-cat $REDIS_ARM >> $1/fohr_andrew.pem
-echo "" >> $1/fohr_andrew.pem
-echo "-----END RSA PRIVATE KEY-----" >> $1/fohr_andrew.pem
+
+echo "-----BEGIN RSA PRIVATE KEY-----" >> ./fohr_andrew.pem
+cat $REDIS_ARM >> ./fohr_andrew.pem
+echo "" >> ./fohr_andrew.pem
+echo "-----END RSA PRIVATE KEY-----" >> ./fohr_andrew.pem
 echo "SSH set-up complete 👍 🤘 👌"
 
 ssh -o "StrictHostKeyChecking=no" -L 6379:fohr-andrew-test-2.dpdkrj.0001.use1.cache.amazonaws.com:6379 -i ./fohr_andrew.pem ubuntu@ec2-54-235-5-112.compute-1.amazonaws.com
