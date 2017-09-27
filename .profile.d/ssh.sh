fcf337f9ce9bd9b3f3db0825fba14b72febbce03
superandrew123 <andrewmiksch@gmail.com>
2017-09-27 10:31:47 -0400

    #yolo push

--- .profile.d/ssh.sh
+++ .profile.d/ssh.sh
@@ -1,1 +1,3 @@
-touch TESTFILEPLEASEGOD.txt
+# open ssh connection to EC2 instance
+
+ssh -o "StrictHostKeyChecking no" -L 6379:fohr-andrew-test-2.dpdkrj.0001.use1.cache.amazonaws.com:6379 -i ./fohr_andrew.pem ubuntu@ec2-54-235-5-112.compute-1.amazonaws.com
