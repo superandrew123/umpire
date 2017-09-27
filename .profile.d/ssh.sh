1f539d052e0e93d24f20383bced5e2e1683151c4
superandrew123 <andrewmiksch@gmail.com>
2017-09-27 11:10:12 -0400

    #yolo push

--- .profile.d/ssh.sh
+++ .profile.d/ssh.sh
@@ -1,6 +1,6 @@
 # open ssh connection to EC2 instance
 
-touch $1/fohr_andrew.pem
+touch ./fohr_andrew.pem
 
 echo "-----BEGIN RSA PRIVATE KEY-----" >> $1/fohr_andrew.pem
 cat $REDIS_ARM >> $1/fohr_andrew.pem
