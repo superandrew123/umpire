c1328d18eab7cd072a1ab1c5b9f8a8080e0dc0ca
superandrew123 <andrewmiksch@gmail.com>
2017-09-28 10:34:25 -0400

    #yolo push

--- .profile.d/ssh.sh
+++ .profile.d/ssh.sh
@@ -10,5 +10,5 @@ $REDIS_ARM
 EOF
 echo "SSH set-up complete 👍 🤘 👌"
 
-# ssh -o "StrictHostKeyChecking=no" -L 6379:fohr-andrew-test-2.dpdkrj.0001.use1.cache.amazonaws.com:6379 -i ./fohr_andrew.pem ubuntu@ec2-54-235-5-112.compute-1.amazonaws.com
+ssh -nNT -o "StrictHostKeyChecking=no" -L 6379:fohr-andrew-test-2.dpdkrj.0001.use1.cache.amazonaws.com:6379 -i ./fohr_andrew.pem ubuntu@ec2-54-235-5-112.compute-1.amazonaws.com
 echo "<---- SSHed ---->"
