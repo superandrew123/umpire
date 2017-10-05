8f9718157bd7dcec6da395bcfb1f36bf9312e2b9
superandrew123 <andrewmiksch@gmail.com>
2017-10-05 17:16:26 -0400

    #yolo push

--- .profile.d/ssh.sh
+++ .profile.d/ssh.sh
@@ -11,5 +11,6 @@ EOF
 echo "SSH set-up complete 👍 🤘 👌"
 
 
+
 # ssh -nNT -o "StrictHostKeyChecking=no" -L 6379:fohr-andrew-test-2.dpdkrj.0001.use1.cache.amazonaws.com:6379 -i ./fohr_andrew.pem ubuntu@ec2-54-235-5-112.compute-1.amazonaws.com
 echo "<---- SSHed ---->"
