377c65a963316cb75630d234ebefb732ac94adda
superandrew123 <andrewmiksch@gmail.com>
2017-10-11 16:36:35 -0400

    #yolo push

--- .profile.d/ssh.sh
+++ .profile.d/ssh.sh
@@ -1,3 +1,3 @@
 # open ssh connection to EC2 instance
 
-# ssh -o "StrictHostKeyChecking no" -f -N -L $REDIS_PORT_INFO -i ~/fohr_andrew.pem $EC2_REDIS_BRIDGE
+ssh -o "StrictHostKeyChecking no" -f -N -L $REDIS_PORT_INFO -i ~/fohr_andrew.pem $EC2_REDIS_BRIDGE
