8e8207384df26ba650bc3d08cc9e9a7d17999660
superandrew123 <andrewmiksch@gmail.com>
2017-10-12 14:41:51 -0400

    #yolo push

--- .profile.d/ssh.sh
+++ .profile.d/ssh.sh
@@ -1,15 +1,11 @@
 # open ssh connection to EC2 instance
 
 touch fohr_andrew.pem
+
 echo "       Creating pem"
 echo "-----BEGIN RSA PRIVATE KEY-----" >> fohr_andrew.pem
-echo $REDIS_ARM
 echo $REDIS_ARM >> fohr_andrew.pem
-# echo "" >> fohr_andrew.pem
 echo "-----END RSA PRIVATE KEY-----" >> fohr_andrew.pem
 echo "       Created pem"
-
-
-
 
 ssh -o "StrictHostKeyChecking no" -f -N -L $REDIS_PORT_INFO -i ~/fohr_andrew.pem $EC2_REDIS_BRIDGE
---
+++ config/initializers/redis.rb
@@ -0,0 +1,3 @@
+# Testing for 
+
+$redis = Redis.new
